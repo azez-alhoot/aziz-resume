fb4ea3fe0941187ce9fe9909a4696915e5e97f61
azez-alhoot <azezalhoot82@yahoo.com>
2021-06-14 02:26:30 +0300

    creating my reume

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// hide and show the skills part
+$(document).ready(function(){
+    $(".skills-show").click(function(){
+      $(".skills").toggle();
+    });
+
+    $(".education-show").click(function(){
+        $(".education").toggle();
+      });
+
+  });

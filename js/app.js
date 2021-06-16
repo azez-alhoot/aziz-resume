b54d2d0a01a78a3926eb00f7278f370d1ae85757
azez-alhoot <azezalhoot82@yahoo.com>
2021-06-16 16:36:38 +0300

    add expereance and projects part

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
 // hide and show the skills part
 $(document).ready(function(){
-    $(".skills-show").click(function(){
-      $(".skills").toggle();
-    });
 
-    $(".education-show").click(function(){
-        $(".education").toggle();
-      });
+  $(".btn-show").click(function(){
+    $(this).next().toggle();
+  })
 
-  });
+});
